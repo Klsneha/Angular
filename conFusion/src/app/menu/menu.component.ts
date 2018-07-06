639015a4c1ffcc9728b0e2b8337dc243b3e77a81
Sneha Kandukuri <kandukurisneha94@gmail.com>
2018-07-05 18:47:35 -0700

    Basic Services

--- conFusion/src/app/menu/menu.component.ts
+++ conFusion/src/app/menu/menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 
 @Component({
@@ -11,16 +11,17 @@ import { DISHES } from '../shared/dishes';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
+  dishes: Dish[];
 
   selectedDish: Dish;
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
-  
+
   onSelect(dish: Dish) {
     this.selectedDish = dish;
   }
