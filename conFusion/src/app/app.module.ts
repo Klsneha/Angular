249ec2026ae837a8044b914a1ae8e821213bc6b8
Sneha Kandukuri <kandukurisneha94@gmail.com>
2018-07-07 18:20:11 -0700

    Single Page Applications Part 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -24,6 +24,8 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -50,7 +52,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [ DishService ],
+  providers: [ DishService , PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
