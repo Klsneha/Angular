d468b20f3cbfdd6a9f28fe56dcf3bd2f4391339e
Sneha Kandukuri <kandukurisneha94@gmail.com>
2018-07-13 09:38:09 -0700

    Reactive Forms

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -36,7 +36,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+import{ ReactiveFormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 @NgModule({
@@ -65,6 +68,9 @@ import { FormsModule } from '@angular/forms';
     MatInputModule,
     MatCheckboxModule,
     FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     AppRoutingModule
   ],
   providers: [ DishService , PromotionService, LeaderService],
